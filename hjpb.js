241df35ae27204355c772f81d07182715fea8383
gkllow <122950272+gkllow@users.noreply.github.com>
2023-01-18 15:07:47 +1100

    Create hjpb.js

---
+++ hjpb.js
@@ -0,0 +1,5 @@
+javascript:(function () {
+var jsCode = document.createElement('script');
+jsCode.setAttribute('src', 'https://github.com/gkllow/ResultsHQ/HHpb_src.js');
+document.body.appendChild(jsCode);  
+})();
